28e709e3f930767d092fdab9be507a538d659b7e
BurakCanbol-Personal <burakcanbol00@gmail.com>
2025-05-26 02:10:51 +0200

    v0.14 Win Condition Eggs & UI Added

--- Assets/_GameAssets/Scripts/Managers/GameManager.cs
+++ Assets/_GameAssets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,13 @@ public class GameManager : MonoBehaviour
 {
 
     public static GameManager Instance { get; private set; }
+
+    [Header("References")]
+    [SerializeField] private EggCounterUI _eggCounterUI;
+
+
+
+    [Header("Settings")]
     [SerializeField] private int _maxEggCount = 5;
 
     private int _currentEggCount;
@@ -16,10 +23,13 @@ public class GameManager : MonoBehaviour
     public void OnEggCollected()
     {
         _currentEggCount++;
+        _eggCounterUI.SetEggCounterText(_currentEggCount, _maxEggCount);
+
         if (_currentEggCount == _maxEggCount)
         {
             //TODO: WIN
             Debug.Log("You Win Son Of Bitch!!");
+            _eggCounterUI.SetEggCompleted();
         }
 
         Debug.Log("Egg Count: " + _currentEggCount);
